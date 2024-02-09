af0353f3b3d440e7fb9cfcdf1f389d962374fce2
tzhf <theo.zahaf@gmail.com>
2024-02-10 00:42:40 +0100

    some refacto

--- src/auth/auth_impl.ts
+++ src/auth/auth_impl.ts
@@ -1,12 +1,8 @@
-// import { supabase } from '../utils/useSupabase'
-
 const startAuth: HTMLButtonElement | null = document.querySelector('#start-auth')
 if (startAuth) {
   startAuth.addEventListener('click', () => {
     startAuth.disabled = true
-    // supabase.auth.signOut().finally(() => {
     // @ts-expect-error TS2304: defined by preload script
     chatguessrApi.startAuth()
-    // })
   })
 }
--- vite.main.config.ts
+++ vite.main.config.ts
@@ -1,18 +1,21 @@
 import { defineConfig } from 'vite'
+import pkg from './package.json'
 
 // https://vitejs.dev/config
 export default defineConfig({
-  resolve: {
-    // Some libs that can run in both Web and Node.js, such as `axios`, we need to tell Vite to build them in Node.js.
-    // @ts-expect-error (browserField is deprecated, but i couldn't get it working with mainFields for now)
-    browserField: false,
-    conditions: ['node'],
-    mainFields: ['module', 'jsnext:main', 'jsnext']
-  },
+  // resolve: {
+  //   // Some libs that can run in both Web and Node.js, such as `axios`, we need to tell Vite to build them in Node.js.
+  //   // @ts-expect-error (browserField is deprecated, but i couldn't get it working with mainFields for now)
+  //   browserField: false,
+  //   conditions: ['node'],
+  //   mainFields: ['module', 'jsnext:main', 'jsnext']
+  // },
   build: {
     rollupOptions: {
-      // if we don't include 'bufferutil', 'utf-8-validate' we get 'Could not resolve "utf-8-validate" imported by "ws"'
-      external: ['better-sqlite3', 'coordinate_to_country', 'bufferutil', 'utf-8-validate']
+      // external: ['better-sqlite3', 'coordinate_to_country', 'bufferutil', 'utf-8-validate'],
+      // Some third-party Node.js libraries may not be built correctly by Vite, especially `C/C++` addons,
+      // we can use `external` to exclude them to ensure they work correctly.
+      external: Object.keys('dependencies' in pkg ? pkg.dependencies : {})
     }
   }
 })
